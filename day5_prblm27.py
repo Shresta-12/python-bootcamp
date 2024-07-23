06e49cfdf473f3518fe042dda544248ed539df03
Shresta-12 <128480968+Shresta-12@users.noreply.github.com>
2024-07-23 20:46:49 +0530

    day5

---
+++ day5_prblm27.py
@@ -0,0 +1,22 @@
+'''string method are is alpha ,is alam, ia upper,is lower,check with lower,converting to upper,title case,swapcase'''
+
+s=input()
+#print(s.upper())---HELLO WORLD
+#print(s.lower())
+#print(s.title())----Hello World
+#print(s.swapcase())---convert the lower to upper 
+#print(s.capitalize())-----Hello world
+#print(s.strip())------remove the unwanted space
+#print(s.replace('a','z'))
+#print(s.split('l'))
+
+#gives true or false
+
+#print(s.isdigit())---- are 0-9
+#print(s.isalpha())---check alphabets and no space then true or false
+#print(s.isalnum())----check alphabeta and numeric
+#print(s.isnumeric())---- all the numbers
+print(s.isascii())
+#print(s.islower())
+#print(s.isupper())
+#print(s.istitle())
