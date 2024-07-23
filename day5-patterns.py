8973f71b1f5b520582f977920a1479840eb4d9fb
Shresta-12 <128480968+Shresta-12@users.noreply.github.com>
2024-07-23 20:50:10 +0530

    day5

---
+++ day5-patterns.py
@@ -0,0 +1,31 @@
+'''print 
+*****
+*****
+*****
+*****
+*****'''
+
+
+for i in range(5):
+    for j in range(5):
+        print("*",end="")
+    print()  
+
+ ''' ****
+    * ***
+    ** **
+    *** *
+    ****  '''   
+for i in range(5):
+    for j in range(5):
+        if(i==j):
+            print(" ",end="")
+        else:
+            print("*",end="")
+    print()  
+
+
+
+
+
+   
